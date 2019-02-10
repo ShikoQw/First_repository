4b576c12db87f0cf9fb315160b148dafe728ad96
Nikita <qw_18@mail.ru>
2019-02-10 12:45:16 +0600

    second commit

--- src/main/java/Main_servlet.java
+++ src/main/java/Main_servlet.java
@@ -1,11 +1,10 @@
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/")
+
 public class Main_servlet extends HttpServlet {
 
   @Override
